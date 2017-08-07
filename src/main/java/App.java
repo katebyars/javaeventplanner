5b1f55e3bdbbbf1d0d7215eceb762ca2e1b75c83
Kate Byars and Stephanie Mayer <kate@>
2017-08-07 13:54:36 -0700

    add css styling.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,10 +9,28 @@ import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         get("/form", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             return new ModelAndView(model, "form.hbs");
         }, new HandlebarsTemplateEngine());
 
+        get("/eventPlanning", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+
+            String beverage = request.queryParams("beverage");
+            String food = request.queryParams("food");
+            String entertainment = request.queryParams("entertainment");
+            int guestCount = Integer.parseInt(request.queryParams("guestCount"));
+            model.put("beverage", beverage);
+            model.put("food", food);
+            model.put("entertainment", entertainment);
+            model.put("guestCount", guestCount);
+            eventPlanning event = new eventPlanning(beverage, food, entertainment, guestCount);
+            int totalPrice = event.priceCalculation(beverage, food, entertainment, guestCount);
+            model.put("totalPrice", totalPrice);
+            return new ModelAndView(model, "eventPlanning.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
+
--- src/main/java/models/eventPlanning.java
+++ src/main/java/models/eventPlanning.java
@@ -1,7 +1,68 @@
 package models;
 
-/**
- * Created by Guest on 8/7/17.
- */
+
 public class eventPlanning {
+    public String getBeverage() {
+        return beverage;
+    }
+
+    private String beverage;
+
+    public String getFood() {
+        return food;
+    }
+
+    private String food;
+
+    public String getEntertainment() {
+        return entertainment;
+    }
+
+    private String entertainment;
+
+    public int getGuestCount() {
+        return guestCount;
+    }
+
+    private int guestCount;
+    public int totalPrice = 0;
+
+
+    public eventPlanning (String beverage, String food, String entertainment, int guestCount) {
+        this.beverage = beverage;
+        this.food = food;
+        this.entertainment = entertainment;
+        this.guestCount = guestCount;
+    }
+
+    public int priceCalculation (String beverage, String food, String entertainment, int guestCount) {
+        if (beverage.equals("Kombucha")){
+            totalPrice += guestCount*5;
+        } else if (beverage.equals("Coffee")){
+            totalPrice += guestCount*3;
+        } else if (beverage.equals("Tea")){
+            totalPrice += guestCount*2;
+        } else {
+            totalPrice += guestCount*1;
+        }
+        if (food.equals("Hamburgers")){
+            totalPrice += guestCount*7;
+        } else if (food.equals("Asian Food")) {
+            totalPrice += guestCount*9;
+        } else if (food.equals("Indian Buffet")){
+            totalPrice += guestCount*11;
+        } else {
+            totalPrice += guestCount*5;
+        }
+        if (entertainment.equals("DJ")){
+            totalPrice += 1000;
+        } else if(entertainment.equals("Live Band")) {
+            totalPrice += 2500;
+        } else if(entertainment.equals("Fire Eater")){
+            totalPrice += 700;
+        } else {
+            totalPrice += 10000;
+        }
+        return totalPrice;
+    }
 }
